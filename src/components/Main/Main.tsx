fcadbcbbce8c321bd2de3600b424697a137f8c3e
kaleab <60797196+kaleab27@users.noreply.github.com>
2023-01-12 04:55:12 +0300

    removed unused useRef import

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React from "react";
 import styles from './Main.module.scss'
 import RadioButton from "../RadioButton/RadioButton";
 import {Link} from "react-router-dom";
